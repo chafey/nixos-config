89cd7001fb8d21228a6110cf24aaa9a18bd6ff28
Chris Hafey <chafey@gmail.com>
2024-05-03 15:24:37 -0500

    added nixify() which initializes a directory with direnv for c++/node dev

--- home-manager-configuration.nix
+++ home-manager-configuration.nix
@@ -17,6 +17,27 @@ in
         enable = true;
         theme = "steeef";
       };
+      initExtra = ''
+nixify() {
+  if [ ! -e ./.envrc ]; then
+    echo "use nix" > .envrc
+    direnv allow
+  fi
+  if [[ ! -e shell.nix ]] && [[ ! -e default.nix ]]; then
+    cat > default.nix <<'EOF'
+with import <nixpkgs> {};
+mkShell {
+  packages = [
+    pkgs.gnumake
+    pkgs.cmake
+    pkgs.clang
+    pkgs.nodejs
+  ];
+}
+EOF
+  fi
+}
+'';
     };
     programs.git = {
       enable = true;
